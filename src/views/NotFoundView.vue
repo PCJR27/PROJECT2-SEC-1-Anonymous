7db2f203e9a37f4d6e220167c17e7342062cc086
Thanawat033 <88100119+Thanawat04@users.noreply.github.com>
2023-04-01 22:54:42 +0700

    update not found view

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -1,9 +1,7 @@
 <template>
-  <div class="w-full max-w-5xl border border-gray-200">
-    <div class="flex flex-col items-center justify-center text-sm h-36 italic">
-      <p class="text-red-500 font-semibold tracking-wider text-xl">
-        Your Page is Not Found
-      </p>
+  <div class="w-screen h-screen content-center grid" style="background-color: #2A303C;">
+    <div>
+      <p class="text-red-500 font-semibold text-6xl text-center">Your Page Is Not Found</p>
     </div>
   </div>
 </template>
